a8932d238e86c37fb2f3079b7e002bcb1704ee10
Abel Arismendy <a.arismendy@uniandes.edu.co>
2022-08-22 16:41:21 -0500

    prueba

--- Proyecto0.py
+++ Proyecto0.py
@@ -10,35 +10,37 @@ Spaces and tabulators are separators and should be ignored (outside of instructi
 import re
 
 name_pattern = r'\w+[\w\d]*'
+var_pattern = rf'^\s*(var|VAR)(\s*{name_pattern},)*\s*{name_pattern};$'
+exp = rf'^(PROG|prog)[\s\n]*{var_pattern}\n*'
 
 def main():
     greatbool = False
-    code = open("Prueba.txt", "r")
+    code = open("prueba2.txt", "r")
     code = code.read()
     code = code.strip()
 
+    parse(code)
     # Verify if the code starts with PROG and ends with GORP.
-    if (code[:4]) == "PROG" and (code[-4:]) == "GORP":
-        code = code[4:]
-        code = code[:-4]
-        greatbool = parse(code)
+    # if (code[:4]) == "PROG" and (code[-4:]) == "GORP":
+    #     code = code[4:]
+    #     code = code[:-4]
+    #     greatbool = parse(code)
 
-    print(greatbool)
+    # print(greatbool)
 
 def parse(code):
     # Parsea el codigo
-    parenthesis = code.count("(") == code.count(")")
-    brackets = code.count("{") == code.count("}")
+    # code = whitecode(code)
+    code2 = code.splitlines()
+    print(code)
+    codigo = re.match(re.compile(exp), code)
+    print(codigo, 'a')
+    for line in code2:
+        variables = declareVariables(line)
+        if variables is not None:
+            print(variables)
 
-    if parenthesis and brackets:
-        # code = whitecode(code)
-        code = code.splitlines()
-        for line in code:
-            variables = declareVariables(line)
-            if variables is not None:
-                print(variables)
-
-            checkProcedure(line)
+        checkProcedure(line)
 
 
 def whitecode(code):
@@ -53,8 +55,8 @@ def whitecode(code):
 
 def declareVariables(line):
     # Revisa las declaraciones de variables
-    var_pattern = re.compile(rf'^\s*(var|VAR)(\s*{name_pattern},)*\s*{name_pattern};$')
-    is_match = re.match(var_pattern, line)
+    is_match = re.match(re.compile(var_pattern), line)
+
     if is_match is not None:
         line = line.strip('var ')
         line = line.strip('VAR ')
@@ -64,10 +66,9 @@ def declareVariables(line):
 
 def checkProcedure(line):
     # Revisa si la definición de un Procedimiento es válida
-    proc_pattern = re.compile(rf'^\s*(PROC|proc)\s*{name_pattern}\s*\((({name_pattern},)*\s*{name_pattern})\)|\s*\)$')
+    proc_pattern = re.compile(rf'^\s*(PROC|proc)\s*{name_pattern}\s*\(((({name_pattern},)*\s*{name_pattern}\))|\s*\))')
     is_match = re.match(proc_pattern, line)
-    if is_match is not None:
-        print(is_match)
+    return bool(is_match)
 
 if __name__ == '__main__':
     main()
