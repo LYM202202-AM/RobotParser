b63c4629dc2bff74a142001bd078879385eb1a3e
Abel Arismendy <a.arismendy@uniandes.edu.co>
2022-08-23 20:36:46 -0500

    conditionals & if patterns

--- Proyecto0.py
+++ Proyecto0.py
@@ -7,15 +7,31 @@ allow recursion.
 Spaces and tabulators are separators and should be ignored (outside of instructions)."""
 
 import re
+from signal import valid_signals
 
 name_pattern = r'\w+[\w\d]*'
 var_pattern = rf'^\s*VAR(\s*{name_pattern},)*\s*{name_pattern};$'
 # exp = rf'^(PROG|prog)[\s\n]*{var_pattern}\n*'
 
+commandslist = ["walk", "jump", "drop", "grab", "get", "free", "pop"]
+orientationList = ["north", "south", "east", "west"]
+directionsList = ["around", "left", "right"]
+walkList = orientationList.copy()
+walkList.extend(["front", "back", "left", "right"])
+
+
+commands = '|'.join(commandslist)
+posibles_orientation = '|'.join(orientationList)
+posibles_directions = '|'.join(directionsList)
+posibles_walks = '|'.join(walkList)
+
+
 def main():
     greatbool = False
     code = open("Prueba.txt", "r")
     code = code.read()
+    code = code.replace('{', '[')
+    code = code.replace('}', ']')
     code = code.strip()
 
     # parse(code)
@@ -52,7 +68,7 @@ def parse(code):
 
         if in_procedure:
             checkCommand(line, parameters, variables)
-        checkAssignment(line)
+        checkCondition(line, parameters, variables)
 
 def declareVariables(line):
     # Revisa las declaraciones de variables
@@ -87,55 +103,68 @@ def checkIns(line):
     pass
 
 def checkCommand(line, parameters, variables):
-    commandslist = ["walk", "jump", "drop", "grab", "get", "free", "pop"]
-    orientationList = ["north", "south", "east", "west"]
-    directionsList = ["around", "left", "right"]
-    walkList = orientationList.copy()
-    walkList.extend(["front", "back", "left", "right"])
 
-
-    commands = '|'.join(commandslist)
     posibles = variables.copy()
     posibles.extend(parameters)
     posibles_parameters = '|'.join(posibles)
-    posibles_orientation = '|'.join(orientationList)
-    posibles_directions = '|'.join(directionsList)
-    posibles_walks = '|'.join(walkList)
 
+    command_pattern = rf'^\s*({commands})\s*\(\s*(({posibles_parameters})|\d+)\s*\)\s*;\s*$'
+    jumpTo_pattern = rf'^\s*jumpTo\s*\(\s*(({posibles_parameters})|\d+)\s*,\s*(({posibles_parameters})|\d+)\s*\)\s*;\s*$'
+    veer_pattern = rf'^\s*veer\s*\(\s*({posibles_directions})\s*\)\s*;\s*$'
+    look_pattern = rf'^\s*look\s*\(\s*({posibles_orientation})\s*\)\s*;\s*$'
+    walk_pattern = rf'^\s*walk\s*\(\s*({posibles_walks})\s*,\s*(({posibles_parameters})|\d+)\s*\)\s*;\s*$'
+    assign_pattern = rf'^\s*({name_pattern})\s*:=\s*\d+\s*;$'
 
-    command_pattern = re.compile(rf'^\s*({commands})\s*\(\s*(({posibles_parameters})|\d+)\s*\)\s*;\s*$')
-    jumpTo_pattern = re.compile(rf'^\s*jumpTo\s*\(\s*(({posibles_parameters})|\d+)\s*,\s*(({posibles_parameters})|\d+)\s*\)\s*;\s*$')
-    veer_pattern = re.compile(rf'^\s*veer\s*\(\s*({posibles_directions})\s*\)\s*;\s*$')
-    look_pattern = re.compile(rf'^\s*look\s*\(\s*({posibles_orientation})\s*\)\s*;\s*$')
-    walk_pattern = re.compile(rf'^\s*walk\s*\(\s*({posibles_walks})\s*,\s*(({posibles_parameters})|\d+)\s*\)\s*;\s*$')
+    global_command_pattern = rf'({command_pattern}|{jumpTo_pattern}|{veer_pattern}|{look_pattern}|{walk_pattern}|{assign_pattern})'
 
-    is_command = re.match(command_pattern, line)
-    is_jumpTo = re.match(jumpTo_pattern, line)
-    is_veer = re.match(veer_pattern, line)
-    is_look = re.match(look_pattern, line)
-    is_walk = re.match(walk_pattern, line)
-
-    if is_command is not None:
-        print(is_command.group(0))
-
-    if is_jumpTo is not None:
-        print(is_jumpTo.group(0))
-
-    if is_veer is not None:
-        print(is_veer.group(0))
-
-    if is_look is not None:
-        print(is_look.group(0))
-
-    if is_walk is not None:
-        print(is_walk.group(0))
-
-def checkAssignment(line):
-    assign_pattern = re.compile(rf'^\s*({name_pattern})\s*:=\s*\d+\s*;$')
-    is_match = re.match(assign_pattern, line)
+    is_match = re.match(global_command_pattern, line)
     if is_match is not None:
         print(is_match.group(0))
 
+
+def checkIf(line, parameters, variables):
+    pass
+    # if_pattern = re.compile(rf'^\s*if\s*({condition})')
+
+def checkCondition(line, parameters, variables):
+
+    posibles = variables.copy()
+    posibles.extend(parameters)
+    posibles_parameters = '|'.join(posibles)
+
+
+    validList = ["walk", "jump", "grab", "pop" "pick", "free", "drop"]
+    canWalkList = ["north", "south", "east", "west", "front", "back", "left", "right"]
+
+    posibles_valid = '|'.join(validList)
+    posibles_canWalk = '|'.join(canWalkList)
+
+    facing_pattern = rf'\s*isfacing\s*\(\s*({posibles_orientation})\s*\)\s*'
+    isvalid_pattern = rf'\s*isValid\s*\(\s*({posibles_valid})\s*,\s*(({posibles_parameters})|\d+)\s*\)\s*'
+    canwalk_pattern = rf'\s*canWalk\s*\(\s*({posibles_canWalk})\s*,\s*(({posibles_parameters})|\d+)\s*\)\s*'
+
+
+    condition_pattern = rf'\s*({facing_pattern}|{isvalid_pattern}|{canwalk_pattern})\s*'
+
+    not_pattern = rf'\s*not\s*\(\s*({condition_pattern})\s*\)\s*'
+
+    global_condition_pattern = rf'\s*({condition_pattern}|{not_pattern})\s*'
+
+    command_pattern = rf'\s*({commands})\s*\(\s*(({posibles_parameters})|\d+)\s*\)\s*'
+    jumpTo_pattern = rf'\s*jumpTo\s*\(\s*(({posibles_parameters})|\d+)\s*,\s*(({posibles_parameters})|\d+)\s*\)\s*'
+    veer_pattern = rf'\s*veer\s*\(\s*({posibles_directions})\s*\)\s*'
+    look_pattern = rf'\s*look\s*\(\s*({posibles_orientation})\s*\)\s*'
+    walk_pattern = rf'\s*walk\s*\(\s*({posibles_walks})\s*,\s*(({posibles_parameters})|\d+)\s*\)\s*'
+    assign_pattern = rf'\s*({posibles_parameters})\s*:=\s*\d+\s*'
+
+    global_command_pattern = rf'({command_pattern}|{jumpTo_pattern}|{veer_pattern}|{look_pattern}|{walk_pattern}|{assign_pattern})'
+
+
+    if_pattern = rf'^\s*if\s*\(({global_condition_pattern})\)\s*\[\s*{global_command_pattern}\s*\]\s*(else\s*\[{global_command_pattern}\])?\s*fi\s*'
+
+    is_match = re.search(if_pattern, line)
+    if is_match is not None:
+        print(is_match.group(0))
 if __name__ == '__main__':
     main()
 
